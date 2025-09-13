0ca476c13fc9400b2322bf058bdc9f2772a03ef5
Jose Andres Solano Vargas <VargasAPI11@gmail.com>
2025-09-13 02:20:05 -0600

    Update: These CSS files have been updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import { FaGithub, FaLinkedin, FaInstagram, FaEnvelope } from "react-icons/fa";
 import "./assets/styles/App.css";
 
 function App() {
   return (
     <>
-      <header>
-        <h1>VargasAPI11</h1>
-      </header>
+      <div className="contenedor">
+        <div className="app-container">
+          <header className="header">
+            <div className="info-personal">
+              <h1>Jose Vargas</h1>
+              <h2>Full Stack Engineer</h2>
+              <p>
+                This is a work experience lab for my Desarrollo de Software
+                course. My goal is to practice my current front-end skills, and
+                I might use this lab as inspiration for my future portfolio.
+              </p>
+              <nav className="nav">
+                <ul>
+                  <li>ABOUT</li>
+                  <li>EXPERIENCE</li>
+                  <li>PROJECTS</li>
+                  <li>TEC COURSE</li>
+                </ul>
+              </nav>
+            </div>
+            <ul className="social-media">
+              <li>
+                <a
+                  href="https://github.com/VargasAPI?tab=repositories"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub size={25} />
+                </a>
+              </li>
+            <li>
+                <a
+                href="https://www.linkedin.com/in/jose-andres-solano-vargas-204149351"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin size={25} />
+              </a>
 
-       <div class="contenedor-navegacion">
-         <Navbar />   {/* Componente nav de component*/}
 
+            </li>
+            
+              <li>
+                <FaInstagram size={25} />
+              </li>
+              <li>
+                <FaEnvelope size={25} />
+              </li>
+            </ul>
+          </header>
 
-       </div>
-     
-      <main>
-        <h1>Bienvenido a mi portafolio 🚀</h1>
-      </main>
+          <main className="right-panel">
+            <section id="about">Sobre mí</section>
+          </main>
+        </div>
+      </div>
     </>
   );
 }
